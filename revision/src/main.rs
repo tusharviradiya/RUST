914f545e27fbc17f15944affcb9c95605386892b
tusharviradiya <tusharviradiya2@gmail.com>
2024-03-06 16:49:34 +0530

    file transfer

--- revision/src/main.rs
+++ revision/src/main.rs
@@ -1,10 +1,3 @@
-fn main() {
-    #[derive(Debug)]
-    enum Gender{
-        male(u8),
-        female(u8),
-        others(u8),
-    }
-    let one = Gender::male(10);
-    println!("{:?}", one);
+fn main(){
+    
 }
