37ea358c04951a89fd7897ba272e5e20b20a7613
tusharviradiya <tusharviradiya2@gmail.com>
2024-03-07 18:02:54 +0530

    file updation

--- 02_sec_doc/syntax_semantics/src/main.rs
+++ 02_sec_doc/syntax_semantics/src/main.rs
@@ -1,4 +1,10 @@
 fn main(){
-    let t:String = String::from("tushar");
-    println!("{}",t);
+    let mut v = vec![1, 3, 5, 7, 11];
+loop {
+    match v.pop() {
+        Some(x) =>  println!("{}", x),
+        None => break,
+    }
+}
+
 }
