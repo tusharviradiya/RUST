fd87a68ef30661a9c179842c47ea596149521a87
tusharviradiya <tusharviradiya2@gmail.com>
2024-04-01 13:51:19 +0530

    file delete

--- free_code_camp/function/target/debug/deps/function-62ae499746779c3b.d
+++
@@ -1,5 +0,0 @@
-/home/tushar/Desktop/RUST/free_code_camp/function/target/debug/deps/function-62ae499746779c3b: src/main.rs
-
-/home/tushar/Desktop/RUST/free_code_camp/function/target/debug/deps/function-62ae499746779c3b.d: src/main.rs
-
-src/main.rs:
--- free_code_camp/function/target/debug/function.d
+++
@@ -1,1 +0,0 @@
-/home/tushar/Desktop/RUST/free_code_camp/function/target/debug/function: /home/tushar/Desktop/RUST/free_code_camp/function/src/main.rs
